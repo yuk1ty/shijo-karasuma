f11a019b3d1b3f7c44da161ff49d8848a503e65d
Yuki <yuki.mul.tiplus@gmail.com>
2017-05-04 16:06:11 +0900

    CSVローダの実装(未完成)

--- webapp/csvloader/csv_loader.py
+++ webapp/csvloader/csv_loader.py
@@ -7,17 +7,3 @@ def load(file_path):
 
     for row in reader:
       print(row)
-
-
-# class CsvLoader:
-#
-#     def __init__(self):
-#
-#
-#       def load(self, file_path):
-#           with open(file_path, 'r') as file:
-#               reader = csv.reader(file)
-#               header = next(reader)
-#
-#               for row in reader:
-#                   print(row)
---
+++ webapp/csvloader/csv_loader_test.py
@@ -0,0 +1,12 @@
+import unittest
+import csv_loader
+
+class CsvLoaderTest(unittest.TestCase):
+
+    def test_load(self):
+        csv_loader.load("data/test.csv")
+
+        self.assertEqual(True, True)
+
+if __name__ == "__main__":
+    unittest.main()
