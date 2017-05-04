1496dd730b9a9e681f2ddb92df9708164df8e373
kmatsu0402 <keita.matsushima@gmail.com>
2017-05-04 17:25:12 +0900

    📊 added test stock api response

--- webapp/views.py
+++ webapp/views.py
@@ -13,3 +13,36 @@ def test_api(request):
 @csrf_exempt
 def do_something(request):
     return JsonResponse({ 'message' : '反応してないの？' })
+
+@csrf_exempt
+def stock(request):
+    return JsonResponse({
+      'id': '1',
+      'name': 'Nikkei 225',
+      'code': 'NI225',
+      'stockData':  [{
+        'id': '1',
+        'date': '2017-05-04',
+        'time': '09:00:00.000',
+        'start': '19400.00',
+        'high': '19500.00',
+        'low': '19200.00',
+        'end': '19450.00',
+      },{
+        'id': '2',
+        'date': '2017-05-04',
+        'time': '12:00:00.000',
+        'start': '194500.00',
+        'high': '19500.00',
+        'low': '19200.00',
+        'end': '19400.00'
+      },{
+        'id': '3',
+        'date': '2017-05-04',
+        'time': '15:00:00.000',
+        'start': '19400.00',
+        'high': '19550.00',
+        'low': '19200.00',
+        'end': '19550.00'
+      }]
+    })
